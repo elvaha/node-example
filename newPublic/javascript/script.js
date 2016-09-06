e4e798224267751790edf135368de3df2855bed2
elvaha <elias.v.hansen@gmail.com>
2016-09-06 09:50:14 +0200

    funny things

--- newPublic/javascript/script.js
+++ newPublic/javascript/script.js
@@ -1,38 +1,40 @@
-(function($){
-    
+(function ($) {
+
     $.ajax({
         type: 'GET',
         url: '/tweet',
-        success: function(data){
-            for(var i = 0; i < data.tweets.length ; i++){
+        success: function (data) {
+            for (var i = 0; i < data.tweets.length; i++) {
                 appendNewTweet(data.tweets[i]);
             }
         }
     });
-    
-    
-    function appendNewTweet(tweet){
+
+
+    function appendNewTweet(tweet) {
         var newTweet = '<div class="tweet-container">' +
-            '<p class="tweet-time">' + new Date(tweet.time).toLocaleDateString() + '</p>' + 
+            '<p class="tweet-time">' + new Date(tweet.time).toLocaleDateString() + '</p>' +
             '<p class="tweet-body">' + tweet.text + '</p>' +
             '</div>'
-        
+
         $('.tweets').prepend(newTweet);
     }
-    
-    $('.btn-tweet').click(function(){
+
+    $('.btn-tweet').click(function () {
         $.ajax({
             type: 'POST',
-            url: '/tweet',
+            url: 'http://localhost:8080/tweet',
             contentType: 'application/json',
-            data: JSON.stringify({tweet : $('#new-tweet').val()}),
-            success: function(data){
+            data: JSON.stringify({
+                tweet: $('#new-tweet').val()
+            }),
+            success: function (data) {
                 appendNewTweet(data);
                 $('#new-tweet').val('');
             }
-        });    
-        
+        });
+
     })
-    
-    
+
+
 })(jQuery)
